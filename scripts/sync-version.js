840992668ff4414b6873005a5ed2536c87f79065
Shintaro Jokagi <61367823+taroj1205@users.noreply.github.com>
2025-06-03 17:04:06 +1200

    chore(sync-version): remove tauri conf (#53)

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -7,12 +7,6 @@ import { join } from "node:path";
 const packageJson = JSON.parse(readFileSync("package.json", "utf8"));
 const version = packageJson.version;
 
-// Update tauri.conf.json
-const tauriConfigPath = join("src-tauri", "tauri.conf.json");
-const tauriConfig = JSON.parse(readFileSync(tauriConfigPath, "utf8"));
-tauriConfig.version = version;
-writeFileSync(tauriConfigPath, `${JSON.stringify(tauriConfig, null, 2)}\n`);
-
 // Update Cargo.toml
 const cargoTomlPath = join("src-tauri", "Cargo.toml");
 const cargoToml = toml.parse(readFileSync(cargoTomlPath, "utf8"));
